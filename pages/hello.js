cc0f9e8d7e22608b079345e91c961ba1b978a650
Bruno José da Silva <bruno.jose@conectt.com.br>
2021-08-29 20:53:02 -0300

    enviando arquivos do nextjs

---
+++ pages/hello.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css';
+
+const Hello = () => {
+    return (
+        <div className={styles.card}>
+        <h1>Tamo aii</h1>
+      
+        </div>
+    )
+}
+
+export default Hello;
---
+++ pages/user.js
@@ -0,0 +1,15 @@
+import styles from '../styles/Home.module.css'
+
+
+
+const User = () => {
+    return (
+        <div className={styles.card}>
+        <h1>Tela de Usuários</h1>
+        Lista de users
+        </div>
+    )
+    
+}
+
+export default User;
