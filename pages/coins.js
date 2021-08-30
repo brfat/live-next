3d922e510cc81a9c803646fbae9ae6724e7f2abc
brfat <brunofatmeiota@gmail.com>
2021-08-29 22:02:14 -0300

    adicionando rota /coins - criptmoedas

---
+++ pages/coins.js
@@ -0,0 +1,14 @@
+const coins = () => {
+    return (
+        <div className="styles.card">
+            <h1>Lista de Criptmoedas</h1>
+            <ul>
+                <li>Bitcoin</li>
+                <li>Ethereum</li>
+                <li>ADA Cardano</li>
+            </ul>
+        </div>
+    )
+}
+
+export default coins;
