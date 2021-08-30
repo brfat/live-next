afa3a6c93eeb2b1417d3a50519371c446474a25d
brfat <brunofatmeiota@gmail.com>
2021-08-29 22:07:00 -0300

    add new list coins

--- pages/coins.js
+++ pages/coins.js
@@ -1,11 +1,15 @@
+import styles from '../styles/Home.module.css'
+
 const coins = () => {
     return (
-        <div className="styles.card">
-            <h1>Lista de Criptmoedas</h1>
+        <div className={styles.card}>
+            <h1>Lista de Criptomoedas</h1>
             <ul>
                 <li>Bitcoin</li>
                 <li>Ethereum</li>
                 <li>ADA Cardano</li>
+                <li>Wbix</li>
+                <li>Litecoin</li>
             </ul>
         </div>
     )
