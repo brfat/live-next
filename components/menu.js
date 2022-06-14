22a4f6121611c0cbf72a2a80247aa4416a60d561
brfat <brunofatmeiota@gmail.com>
2022-06-13 22:00:53 -0300

    removendo item de inicio do menu principal

--- components/menu.js
+++ components/menu.js
@@ -4,7 +4,6 @@ import styles from "../styles/Home.module.css"
 function Menu() {
     return (
         <ul class={styles.menu}>
-            <li><a href="/">Início</a></li>
             <li><a href="/categoria/novidades">Novidades</a></li>
             <li><a href="/categoria/economia">Economia</a></li>
             <li><a href="/categoria/guia-do-investidor">Guia do Investidor</a></li>
